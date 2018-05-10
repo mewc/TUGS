f6b9bdeff0e3b3a19e8d77b85a7c062e69228efa
Ramzi Hossari <ramzi7000@hotmail.com>
2018-05-10 13:42:04 +1000

    fixed the vertical action buttons bug

--- src/js/SubjectCard.js
+++ src/js/SubjectCard.js
@@ -105,7 +105,7 @@ class SubjectCard extends Component {
             </CardText>
             <Divider/>
             <CardActions expandable={true} style={{backgroundColor: "#eae9ea"}}>
-                <div style={{display: "inline"}}>
+                <div style={{display: "flex", "justify-content": "center"}}>
                     <IconButton
                         onClick={this.handleStarClick.bind(this)}
                         disabled={this.props.userId === 7357}>
